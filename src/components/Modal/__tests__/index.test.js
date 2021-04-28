c074b99667c1084b15d27db3fd68b25e875f7438
albenchris <albenchris00@gmail.com>
2021-04-28 17:15:04 -0500

    skeleton of Modal

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Modal from '..';
+
+afterEach(cleanup);
+
+describe('Modal is rendering', () => {
+
+});
